56f59efccd4386b0e236d9a0df76910d3b860741
mika <mail@deadcells.org>
2015-07-14 22:28:06 +0200

    Added ability to search through a file where each word is on a seperate line with 2 dictionaries

--- 223-e/223-e.py
+++ 223-e/223-e.py
@@ -8,7 +8,26 @@ def garland(word):
     degree = garlandDegree(word)
     print (word + " // " + str(degree) + " // " + word+word[degree:]*4)
 
+def garlandFile(file, description):
+    result = ""
+    garland = 0
+    with open(file) as file:
+        for line in file:
+            a = line.strip()
+            g = garlandDegree(a)
+            if(g >garland):
+                result, garland = a, g
+    if(garland>0):
+        print ("Largest garland word in '" + description + "' is " + result)
+    else:
+        print ("No garland words found in '" + description + "'")
+
 garland("programmer")
 garland("ceramic")
 garland("onion")
 garland("alfalfa")
+
+#Both dictionaries sourced from Leipzig Uni's Wortschatz
+#http://wortschatz.uni-leipzig.de/
+garlandFile('english.txt', 'Top 10,000 English Words')
+garlandFile('german.txt', 'Top 10,000 German Words')
