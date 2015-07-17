033d0fb20bebf0136d1a68c1645bc2bdf688c35a
mika <mail@deadcells.org>
2015-07-17 23:13:56 +0200

    Added solution for the 1st optional problem

--- 223-i/223-i.py
+++ 223-i/223-i.py
@@ -5,8 +5,18 @@ def canBeOffensive(word, insult):
             wordResult.remove(c)
     return insult == ''.join(wordResult)
 
+def problemCount(file, insult):
+    count = 0
+    with open(file) as file:
+        for line in file:
+            count+= int(canBeOffensive(line.strip(), insult))
+    print('Problem count for ' + insult + ' is ' + str(count))
+
 print(canBeOffensive("synchronized", "snond"))
 print(canBeOffensive("misfunctioned", "snond"))
 print(canBeOffensive("mispronounced", "snond"))
 print(canBeOffensive("shotgunned", "snond"))
 print(canBeOffensive("snond", "snond"))
+
+problemCount('enable1.txt', 'snond')
+problemCount('enable1.txt', 'rrizi')
